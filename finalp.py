7319abadcacd810556f4bf0edc10a8c35b0d7c70
vishyj <47131725+vishyj@users.noreply.github.com>
2019-08-09 13:56:55 -0500

    Add files via upload

---
+++ finalp.py
@@ -0,0 +1,218 @@
+Score = 0
+print("Which of these US presidents appeared on the television series LaughIn?")
+print("A: Lyndon Johnson")
+print("B: Richard Nixon")
+print("C: Jimmy Carter")
+print("D: Gerald Ford")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+
+
+print("What is the first letter in the English Alphabet")
+print("A: C")
+print("B: D")
+print("C: A")
+print("D: B")
+x = input(">>>")
+if x == "C":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "B" or x == "D":
+	print("Wrong")
+
+
+
+print("The Earth is approximately how many miles away from the Sun?")
+print("A: 9.3 million")
+print("B: 39 million")
+print("C: 93 million")
+print("D: 193 million")
+x = input(">>>")
+if x == "C":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "B" or x == "D":
+	print("Wrong")
+
+
+
+print("Who won a million dollors")
+print("A: Eric Dem")
+print("B: John Carpenter")
+print("C: Jimmy Carter")
+print("D: Denis Eric")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+
+  
+
+print("Is Bright Side fun")
+print("A: No")
+print("B: Yes")
+print("C: Really FUN")
+print("D: He is a joke")
+x = input(">>>")
+if x == "D":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "B":
+	print("Wrong")
+
+
+
+
+
+
+print("What is better")
+print("A: Fortnite")
+print("B: Minecraft")
+print("C: Apex Legend")
+print("D: Undertale")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+
+
+
+
+
+
+
+
+
+
+print("Best MARVEL Hero")
+print("A: Spider-Man")
+print("B: Friendly fire")
+print("C: Donald Trump")
+print("D: Mike Swag")
+x = input(">>>")
+if x == "A":
+	print("Correct")
+	Score = Score + 1
+elif x == "B" or x == "C" or x == "D":
+	print("Wrong")
+
+
+
+print("Best DC Hero")
+print("A: Batman")
+print("B: The Flash")
+print("C: Arrow")
+print("D: Shazam")
+x = input(">>>")
+if x == "A":
+	print("Correct")
+	Score = Score + 1
+elif x == "B" or x == "C" or x == "D":
+	print("Wrong")
+
+
+
+
+print("Are you smart?")
+print("A: No")
+print("B: Duh")
+print("C: Yes")
+print("D: Frick No!!")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+
+print("Who is Donald Trump?")
+print("A: A janitor")
+print("B: The president")
+print("C: A Local Butcher")
+print("D: A Butler")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+print("Who is Donald Trump?")
+print("A: A janitor")
+print("B: The president")
+print("C: A Local Butcher")
+print("D: A Butler")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+print("Who is Bill Clinton?")
+print("A: A janitor")
+print("B: One of the presidents")
+print("C: A Local Butcher")
+print("D: A Butler")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+print("Who is Nixon")
+print("A: A janitor")
+print("B: One of the presidents")
+print("C: A Local Butcher")
+print("D: A Butler")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+print("Who is Thomas Jefferson?")
+print("A: A janitor")
+print("B: One of the presidents")
+print("C: A Local Butcher")
+print("D: A Butler")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+
+
+print("Do you deserve a million dollors")
+print("A: Yes")
+print("B: Duh")
+print("C: No")
+print("D: Frick No!!")
+x = input(">>>")
+if x == "B":
+	print("Correct")
+	Score = Score + 1
+elif x == "A" or x == "C" or x == "D":
+	print("Wrong")
+if Score == 15:
+	print("You are a millionare")
+elif Score == 11:
+	print("You are a millionare")
+elif Score < 11:
+	print("You are not a millionaire!")
+
